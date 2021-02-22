6f5061dd737258fb7bfd939007e4718dd2963623
devapath89 <72752461+devapath89@users.noreply.github.com>
2021-02-22 11:30:33 +0100

    Initial commit

---
+++ spredsheet.js
@@ -0,0 +1,5 @@
+function multiplyBy()
+{
+        num1 = document.getElementById("LVL").value;
+        num2 = document.getElementById("Growth").value;
+        document.getElementById("result").innerHTML = Lvl * Growth;
