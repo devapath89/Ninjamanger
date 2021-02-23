1d1a566dab12055d8ecd381c30177621effa53e0
devapath89 <72752461+devapath89@users.noreply.github.com>
2021-02-23 14:22:55 +0100

    d

--- spredsheet.js
+++ spredsheet.js
@@ -1,5 +1,33 @@
+
+
+
 function multiplyBy()
 {
-        num1 = document.getElementById("LVL").value;
-        num2 = document.getElementById("Growth").value;
-        document.getElementById("result").innerHTML = Lvl * Growth;
+        var Lvl = parseInt(document.getElementById("LVL").value);
+     
+
+        for(let obj of testModel){      
+        for(let singleStat in obj.Stats){
+
+                for(let growth in obj.Growth ){
+                       if(singleStat == growth){
+                        console.log("Match! Yas!", growth)
+                        //plukke ut verdier fra keys  (navnet på feltet) og gange og plusse og alt det der
+
+                        let statValue = obj.Stats[singleStat];
+                        let growthValue = obj.Growth[growth];
+
+                        console.log(statValue[singleStat] + " test  "+ growthValue[growth])
+
+                        //console.log(isNaN(statValue) , "<- isNAN check  typeof: " , typeof(statValue)  )
+                        document.getElementById("result").innerHTML += `<li>${(growthValue * Lvl) + statValue}</li>`;
+                       }
+                }
+        }
+
+        
+       // document.getElementById("result").innerHTML += `<li>${(growth * Lvl) + stat}</li>`;
+
+        }
+        
+}       
